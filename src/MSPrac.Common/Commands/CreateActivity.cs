43501b2a9f1b66bacbb7dc3aae7990c08e3580fb
John Gallagher <JGallagher585@gmail.com>
2018-07-06 15:54:51 -0600

    fixed dumb error

--- src/MSPrac.Common/Commands/CreateActivity.cs
+++ src/MSPrac.Common/Commands/CreateActivity.cs
@@ -10,7 +10,7 @@ namespace MSPrac.Common.Commands
 
         public string Category { get; set; }
 
-        public string Name { get set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
